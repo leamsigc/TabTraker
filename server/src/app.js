e97d868a95c4f1b6350755e65eaa0fe71c851d1c
ismaelGc <ismaelgc@pop-os.localdomain>
2019-06-26 21:50:23 -0400

    create a simple server

---
+++ server/src/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('combined'));
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send({
+    message: 'Hello from the app',
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
+});
