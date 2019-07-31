404cf135d20315fb0954fe9958ff8b64140be4b0
leamsigc <leamsigc@hotmail.com>
2019-07-31 13:27:51 -0400

    check request and add user and errors

--- server/src/Controllers/AuthenticationController.js
+++ server/src/Controllers/AuthenticationController.js
@@ -12,4 +12,32 @@ module.exports = {
       });
     }
   },
+  async login(req, res) {
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({
+        where: {
+          email,
+        },
+      });
+      if (!user) {
+        return res.status(403).send({
+          error: 'Login information was incorrect ',
+        });
+      }
+      const isPasswordValid = password === user.password;
+      if (!isPasswordValid) {
+        return req.status(403).send({
+          error: 'Please check your password',
+        });
+      }
+      return res.send({
+        user: user.toJSON(),
+      });
+    } catch (err) {
+      return res.status(500).send({
+        error: 'Login information was incorrect... ',
+      });
+    }
+  },
 };
--- server/src/Routes/Routes.js
+++ server/src/Routes/Routes.js
@@ -8,4 +8,5 @@ module.exports = (app) => {
     });
   });
   app.post('/register', AuthenticationControllerPolicy.register, AuthenticationController.register);
+  app.post('/login', AuthenticationController.login);
 };
